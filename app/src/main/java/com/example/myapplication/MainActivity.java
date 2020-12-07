2acc4b1436d7f6f56ff26ffcc3122b94376d867a
Aleksander-Dementev <301692@gmail.com>
2020-12-07 15:44:05 +0300

    Версия на 03.12.2020

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,27 +15,31 @@ import java.io.Serializable;
 
 //фрагменты
 public class MainActivity extends AppCompatActivity {
-    //с 12-19 код по соединению 2-х окон Activity_main и fragment_user
+    FragmentManager fragmentManager = getSupportFragmentManager(); //создаем FragmentManager он нужен для управления фрагментами с помощью функции  getSupportFragmentManager
+
+    //код по соединению 2-х окон Activity_main и fragment_user
     @Override
     protected void onCreate(Bundle savedInstanceState) { //метод onCreate создающий активность
+        Fragment fragment = new UserListFragment(); // Задаем сам фрагмент (UserListFragment-класс фрагмента)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // метод onCreate отображает из файл activity_main (является дизайном нашего приложения)
-
-        FragmentManager fragmentManager = getSupportFragmentManager(); //создаем FragmentManager он нужен для управления фрагментами с помощью функции  getSupportFragmentManager
-        Fragment fragment = new UserListFragment(); // Задаем сам фрагмент (UserListFragment-класс фрагмента)
-        fragmentManager.beginTransaction().add(R.id.fagmentCotainer, fragment).commit(); //берем fragmentManager и вывызаем в нем метод beginTransaction- Помещает элементы на экран. Через медод add показываем где отображать фрагмент, а вторым аргкментом кладем фрагмент который планируем размещат + commit что бы зафексировать  --> 1434_02_12_2020 49:42
+        fragmentManager.beginTransaction().add(R.id.fagmentCotainer, fragment, "main_fragment").addToBackStack("main_fragment").commit(); //берем fragmentManager и вывызаем в нем метод beginTransaction- Помещает элементы на экран. Через медод add показываем где отображать фрагмент, а вторым аргкментом кладем фрагмент который планируем размещат + commit что бы зафексировать  --> 1434_02_12_2020 49:42
     }   //R.id.fragmentCotainer это фрагмент из файла  activity_main.xml
     //UserListFragment размещается на fagmentCotainer
 
 
     @Override
     public void onBackPressed(){
-      /*  super.onBackPressed();*/
-        startActivity(new Intent(MainActivity.this, MainActivity.class));
+        Fragment currentFragment = fragmentManager.findFragmentByTag("main_fragment");//currentFragment - текущий фрагмент присутствующий на экране
+     if (currentFragment != null && currentFragment.isVisible()) {
+         super.onBackPressed();
+     }else {
+         fragmentManager.beginTransaction().replace(R.id.fagmentCotainer, new UserListFragment(), "main_fragment").commit();
+     }
+     /*   startActivity(new Intent(MainActivity.this, MainActivity.class));*/
+
+
     }
-
-
-
     public static void changeFragment(View view, User user){
         // Получаем хостинговую активность (в нашем  случае MianActivity )
         FragmentActivity activity = (FragmentActivity) view.getContext();
